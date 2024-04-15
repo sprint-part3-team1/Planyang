1210a3ef7abc415b3a2ba021fad23096cea2b6fd
Kim Min Jeong <minjeong9919@naver.com>
2024-04-15 14:21:15 +0900

    feat: 기본 픽셀을 10pixel 로 수정
    - 기준 픽셀을 10픽셀로 수정하였습니다.
    - 대표적인 font size 몇 개 뽑아놓았습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
-import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
