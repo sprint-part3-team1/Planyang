c67b9413ed55be678cedd97e03e4114d8e95478b
beom <gb3187@naver.com>
2024-04-16 16:01:01 +0900

    feat : 화살표 버튼 구현

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -1,5 +1,6 @@
 import AddColumnButton from '@/app/_components/Button/AddColumnButton/AddColumnButton';
 import AddTodoButton from '@/app/_components/Button/AddTodoButton/AddTodoButton';
+import ArrowButton from '@/app/_components/Button/ArrowButton/ArrowButton';
 import ConfirmButton from '@/app/_components/Button/ConfirmButton/ConfirmButton';
 import DeleteButton from '@/app/_components/Button/DeleteButton/DeleteButton';
 import DeleteDashBoardButton from '@/app/_components/Button/DeleteDashBoardButton/DeleteDashBoardButton';
@@ -12,8 +13,10 @@ export default function Main() {
       <ConfirmButton /> */}
       {/* <DeleteButton />
       <AddColumnButton /> */}
-      <AddTodoButton />
-      <DeleteDashBoardButton />
+      {/* <AddTodoButton />
+      <DeleteDashBoardButton /> */}
+      &nbsp;
+      <ArrowButton />
     </>
   );
 }
