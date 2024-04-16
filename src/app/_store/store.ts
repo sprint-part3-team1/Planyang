7e9f6416772bef9ea8c1a8e39fcd09fbf55cfba3
JeongSabibi <jsbb528@naver.com>
2024-04-17 01:45:21 +0900

    feat: Columns 생성, 조회, 수정, 삭제 기능 구현

--- src/app/_store/store.ts
+++ src/app/_store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import registerDataReducer from '../_slice/registerSlice';
 import loginDataReducer from '../_slice/loginSlice';
+import columnDataReducer from '../_slice/columnSlice';
 
 const store = configureStore({
   reducer: {
     regsiterData: registerDataReducer,
     loginData: loginDataReducer,
+    columnData: columnDataReducer,
   },
 });
 
