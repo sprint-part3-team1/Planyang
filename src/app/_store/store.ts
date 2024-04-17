9cb221796d5eafdfaa3244dde0616890ff794f69
JeongSabibi <jsbb528@naver.com>
2024-04-18 04:32:50 +0900

    feat: 대시보드 Members 목록 조회, 삭제 기능 구현

--- src/app/_store/store.ts
+++ src/app/_store/store.ts
@@ -4,6 +4,7 @@ import registerDataReducer from '../_slice/registerSlice';
 import loginDataReducer from '../_slice/loginSlice';
 import columnDataReducer from '../_slice/columnSlice';
 import dashBoardReducer from '../_slice/dashBoardSlice';
+import memberReducer from '../_slice/memberSlice';
 
 const store = configureStore({
   reducer: {
@@ -11,6 +12,7 @@ const store = configureStore({
     loginData: loginDataReducer,
     columnData: columnDataReducer,
     dashBoardData: dashBoardReducer,
+    memberData: memberReducer,
   },
 });
 
