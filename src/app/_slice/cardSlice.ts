a6a8e29af5f4c894dda040a8778251e2ccca7f50
JeongSabibi <jsbb528@naver.com>
2024-04-18 02:45:17 +0900

    feat: Cards 생성, 목록 조회, 수정, 상세 조회, 삭제 기능 구현

--- src/app/_slice/cardSlice.ts
+++ src/app/_slice/cardSlice.ts
@@ -9,37 +9,47 @@ interface CardPayload {
   columnId: number;
   title: string;
   description: string;
-  dueDate: string;
-  tags: [string];
-  imageUrl: string;
+  dueDate: string | null;
+  tags: [string] | null;
+  imageUrl: string | null;
 }
 
 interface CardResponseType {
-  data: {
+  id: number;
+  title: string;
+  description: string;
+  tags: [string] | null;
+  dueDate: string | null;
+  assignee: {
+    profileImageUrl: string | null;
+    nickname: string;
     id: number;
-    title: string;
-    description: string;
-    tags: [string];
-    dueDate: string;
-    assignee: {
-      profileImageUrl: string;
-      nickname: string;
-      id: number;
-    };
-    imageUrl: string;
-    teamId: string;
-    columnId: number;
-    createdAt: string;
-    updatedAt: string;
+  };
+  imageUrl: string | null;
+  teamId: string;
+  columnId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface CardActionResponseType {
+  data: CardResponseType | null;
+}
+
+interface GetCardsResponseType {
+  data: {
+    cards: CardResponseType[];
+    totalCount: number;
+    cursorId: number;
   } | null;
 }
 
-const initialState: CardResponseType = {
+const initialState: CardActionResponseType & GetCardsResponseType = {
   data: null,
 };
 
-const asynchFetchCreateCard = createAsyncThunk(
-  'cardSlice/asynchFetchCreateCard',
+const asyncFetchCreateCard = createAsyncThunk(
+  'cardSlice/asyncFetchCreateCard',
 
   async (cardData: CardPayload) => {
     const {
@@ -52,7 +62,7 @@ const asynchFetchCreateCard = createAsyncThunk(
       tags,
       imageUrl,
     } = cardData;
-
+    const accessToken = localStorage.getItem('accessToken');
     const response = await axios.post(
       'https://sp-taskify-api.vercel.app/4-1/cards',
       {
@@ -65,7 +75,107 @@ const asynchFetchCreateCard = createAsyncThunk(
         tags,
         imageUrl,
       },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     );
+    console.log(response.data);
+    return response.data;
+  },
+);
+
+const asyncFetchGetCards = createAsyncThunk(
+  'cardSlice/asyncFetchGetCards',
+
+  async (cardData: CardPayload) => {
+    const { columnId } = cardData;
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await axios.get(
+      `https://sp-taskify-api.vercel.app/4-1/cards?size=200&columnId=${columnId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log(response.data);
+    return response.data;
+  },
+);
+
+const asyncFetchPutCard = createAsyncThunk(
+  'cardSlice/asyncFetchPutCard',
+
+  async (cardData: CardResponseType) => {
+    const {
+      columnId,
+      assignee: { id },
+      id: cardId,
+      title,
+      description,
+      dueDate,
+      tags,
+      imageUrl,
+    } = cardData;
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await axios.put(
+      `https://sp-taskify-api.vercel.app/4-1/cards/${cardId}`,
+      {
+        columnId,
+        assignee: { id },
+        title,
+        description,
+        dueDate,
+        tags,
+        imageUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log(response.data);
+    return response.data;
+  },
+);
+
+const asyncFetchGetCard = createAsyncThunk(
+  'cardSlice/asyncFetchGetCard',
+
+  async (cardData: CardResponseType) => {
+    const { id } = cardData;
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await axios.get(
+      `https://sp-taskify-api.vercel.app/4-1/cards/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log(response.data);
+    return response.data;
+  },
+);
+
+const asyncFetchDeleteCard = createAsyncThunk(
+  'cardSlice/asyncFetchDeleteCard',
+
+  async (cardData: CardResponseType) => {
+    const { id } = cardData;
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await axios.delete(
+      `https://sp-taskify-api.vercel.app/4-1/cards/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log(response.data);
     return response.data;
   },
 );
@@ -76,17 +186,49 @@ const cardSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(
-      asynchFetchCreateCard.fulfilled,
-      (state, action: PayloadAction<CardResponseType['data']>) => {
+      asyncFetchCreateCard.fulfilled,
+      (state, action: PayloadAction<CardActionResponseType['data']>) => {
+        if (state.data && state.data.cards) {
+          state.data.cards.unshift(action.payload);
+          if (state.data.totalCount !== null) {
+            state.data.totalCount++;
+          }
+        }
+      },
+    );
+    builder.addCase(
+      asyncFetchGetCards.fulfilled,
+      (state, action: PayloadAction<GetCardsResponseType['data']>) => {
         state.data = action.payload;
       },
     );
+    builder.addCase(
+      asyncFetchPutCard.fulfilled,
+      (state, action: PayloadAction<CardActionResponseType['data']>) => {
+        state.data = action.payload;
+      },
+    );
+    builder.addCase(
+      asyncFetchGetCard.fulfilled,
+      (state, action: PayloadAction<CardActionResponseType['data']>) => {
+        state.data = action.payload;
+      },
+    );
+    builder.addCase(asyncFetchDeleteCard.fulfilled, (state, action) => {
+      if (state.data && state.data.totalCount !== null) {
+        state.data.totalCount--;
+      }
+    });
   },
 });
 
 export default cardSlice.reducer;
 export const cardActions = {
   ...cardSlice.actions,
-  asynchFetchCreateCard,
+  asyncFetchCreateCard,
+  asyncFetchGetCards,
+  asyncFetchPutCard,
+  asyncFetchGetCard,
+  asyncFetchDeleteCard,
 };
 export const cardData = (state: RootState) => state.cardData.data;
