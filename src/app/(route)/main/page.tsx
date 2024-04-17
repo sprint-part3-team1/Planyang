722f0736f864c6f6c98c2e63fbf3f6c830535b4d
beom <gb3187@naver.com>
2024-04-17 14:39:02 +0900

    feat : TableMember 컴포넌트 구현

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -1,1 +1,11 @@
-export default function Main() {}
+import TableMember from '@/app/(route)/mydashboard/_components/TableMember';
+import styles from './page.module.css';
+
+export default function Main() {
+  return (
+    <div className={styles.container}>
+      <br />
+      <TableMember />
+    </div>
+  );
+}
