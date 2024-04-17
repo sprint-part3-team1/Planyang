10e2c2ea054d1d68cd3a54172dbd8680323d02b9
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-04-18 00:03:21 +0900

    feat : #56 대시보드 유저 초대 기능,내가 초대한 대시보드 목록 api 리덕스로 관리

--- src/app/_store/store.ts
+++ src/app/_store/store.ts
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import registerDataReducer from '../_slice/registerSlice';
 import loginDataReducer from '../_slice/loginSlice';
 import dashBoardReducer from '../_slice/dashBoardSlice';
+import invitationReducer from '../_slice/invitationSlice';
 
 const store = configureStore({
   reducer: {
     regsiterData: registerDataReducer,
     loginData: loginDataReducer,
     dashBoardData: dashBoardReducer,
+    invitationData: invitationReducer,
   },
 });
 
