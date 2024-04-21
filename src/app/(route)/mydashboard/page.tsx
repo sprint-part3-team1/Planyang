8cfe87d44dc9f1ef9a6595464cdd29ff7547c9f8
beom <gb3187@naver.com>
2024-04-21 18:02:46 +0900

    feat : mydashboard 페이지 유저정보 가져오기 및 prop에 전달

--- src/app/(route)/mydashboard/page.tsx
+++ src/app/(route)/mydashboard/page.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { useEffect } from 'react';
+import useAppDispatch from '@/app/_hooks/useAppDispatch';
+import useAppSelector from '@/app/_hooks/useAppSelector';
+import { registerActions, userResponse } from '@/app/_slice/registerSlice';
 import DashboardListNavBar from '@/app/_components/_navbar/_dashboardNavbar/_dashboardListType/DashboardListNavBar';
 import AddDashBoardButton from '@/app/_components/Button/AddDashBoardButton/AddDashBoardButton';
 import SideMenu from './_components/SideMenu';
@@ -5,9 +11,20 @@ import styles from './page.module.css';
 import DashInvite from './_components/DashInvite';
 
 export default function MyDashBoard() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(registerActions.asynchFetchgetUserInfo());
+  }, []);
+
+  // 현재 로그인한 유저의 정보가 담긴 데이터 입니다 getMyInformation 함수를 통해 데이터를 불러옵니다
+  const userData = useAppSelector(userResponse);
+
   return (
     <div>
-      <DashboardListNavBar nickname="박기범" />
+      <DashboardListNavBar
+        nickname={userData.nickname}
+        profileImageUrl={userData?.profileImageUrl}
+      />
       <SideMenu />
       <div className={styles.content}>
         <AddDashBoardButton />
