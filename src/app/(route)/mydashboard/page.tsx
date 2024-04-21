6f7e71e83071fed1d46eb76053c8aece2daa6631
beom <gb3187@naver.com>
2024-04-21 16:15:41 +0900

    feat : mydashboard페이지 전체적인 디자인 구성

--- src/app/(route)/mydashboard/page.tsx
+++ src/app/(route)/mydashboard/page.tsx
@@ -1,5 +1,18 @@
+import DashboardListNavBar from '@/app/_components/_navbar/_dashboardNavbar/_dashboardListType/DashboardListNavBar';
+import AddDashBoardButton from '@/app/_components/Button/AddDashBoardButton/AddDashBoardButton';
 import SideMenu from './_components/SideMenu';
+import styles from './page.module.css';
+import DashInvite from './_components/DashInvite';
 
 export default function MyDashBoard() {
-  return <SideMenu />;
+  return (
+    <div>
+      <DashboardListNavBar nickname="박기범" />
+      <SideMenu />
+      <div className={styles.content}>
+        <AddDashBoardButton />
+        <DashInvite />
+      </div>
+    </div>
+  );
 }
