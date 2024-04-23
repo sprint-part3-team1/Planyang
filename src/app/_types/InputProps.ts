01154b29eaa374ea6a5d4f3c5d8c990b554a094e
Sfer7 <forever_kaka@naver.com>
2024-04-23 23:27:12 +0900

    feat: 로그인 페이지 구현

--- src/app/_types/InputProps.ts
+++ src/app/_types/InputProps.ts
@@ -1,8 +1,13 @@
+import React, {ChangeEventHandler} from "react";
+
 export interface InputProps {
+  readonly inputId: string;
   readonly inputName: string;
   readonly inputType: 'text' | 'password' | 'calendar' | 'tag';
-  readonly inputWidth: number;
+  readonly inputWidth: number | '100%';
+  readonly inputRef: React.MutableRefObject<any>;
   readonly errorMessage?: string | null;
+  readonly onChange?: ChangeEventHandler<HTMLInputElement>;
 
   errorState?: boolean;
   placeholder?: string;
