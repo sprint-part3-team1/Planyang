fb462a2e79af55f5a91a0ffb071774e89a279c07
JeongSabibi <jsbb528@naver.com>
2024-04-24 17:23:26 +0900

    feat: 대시보드에서 새로운 컬럼 추가하는 기능 구현

--- src/app/_components/modal/newColumnModal/NewColumnModal.tsx
+++ src/app/_components/modal/newColumnModal/NewColumnModal.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ModalContainer from '@/app/_components/modal/modalContainer/ModalContainer';
 import VIEWPORT_TYPES from '@/app/constants/viewPortTypes';
 import useGetViewportSize from '@/app/_hooks/useGetViewportSize';
 import { ModalPropsType } from '@/app/_types/modalProps';
 import Input from '@/app/_components/Input';
+import useAppDispatch from '@/app/_hooks/useAppDispatch';
+import { useParams } from 'next/navigation';
+import { columnActions, columnData } from '@/app/_slice/columnSlice';
 import CheckCancleButton from '../checkCancleButton/CheckCancleButton';
 
 const NewColumnModal = ({ setOpenModalType }: ModalPropsType) => {
+  const dispatch = useAppDispatch();
+  const params = useParams();
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  const createColumn = async (title: string, dashboardId: number) => {
+    try {
+      await dispatch(
+        columnActions.asyncFetchCreateColumn({
+          title,
+          dashboardId,
+        }),
+      );
+    } catch (error) {
+      console.error('Error create column:', error);
+    }
+  };
+
   const produceButtonHandler = () => {
-    /** 생성 버튼을 누르면 실행되는 함수 작성 */
+    if (titleRef.current?.value === '') return;
+
+    createColumn(titleRef.current?.value, Number(params.id));
+    setOpenModalType('');
   };
 
   const INPUT_WIDTH = {
@@ -25,6 +48,7 @@ const NewColumnModal = ({ setOpenModalType }: ModalPropsType) => {
         inputName="이름"
         inputType="text"
         inputWidth={INPUT_WIDTH[viewPortType]}
+        inputRef={titleRef}
         errorState={false}
       />
       <CheckCancleButton
