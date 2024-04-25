326a45a4c475b0212988b0cfccc529e69db58d20
Kim Min Jeong <minjeong9919@naver.com>
2024-04-25 15:00:49 +0900

    refactor: side menu 포지션 값 제거 및 마이페이지 layout 추가

---
+++ src/app/(route)/mypage/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import useAppDispatch from '@/app/_hooks/useAppDispatch';
+import useAppSelector from '@/app/_hooks/useAppSelector';
+import { dashBoardActions, dashBoardData } from '@/app/_slice/dashBoardSlice';
+import SideMenu from '../mydashboard/_components/SideMenu';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const dispatch = useAppDispatch();
+
+  // 현재 로그인한 유저의 정보가 담긴 데이터 입니다 getMyInformation 함수를 통해 데이터를 불러옵니다
+  const dashBoardDatas = useAppSelector(dashBoardData);
+
+  // 내가 초대를 받은 목록의 데이터입니다
+
+  useEffect(() => {
+    dispatch(dashBoardActions.asynchFetchGetDashBoard(1));
+  }, [dispatch]);
+  return (
+    <div style={{ display: 'flex' }}>
+      <SideMenu dashBoardData={dashBoardDatas?.dashboards} />
+      {children}
+    </div>
+  );
+}
