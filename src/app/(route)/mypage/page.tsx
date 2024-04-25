0b52c3a50cbc5a75e000f45027119a89d874b616
Kim Min Jeong <minjeong9919@naver.com>
2024-04-25 15:39:43 +0900

    feat: 로딩 페이지 구현

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,29 +1,26 @@
 'use client';
 
 import React, { useEffect } from 'react';
+import { Suspense } from 'react';
 import VIEWPORT_TYPES from '@/app/constants/viewPortTypes';
 import useGetViewportSize from '@/app/_hooks/useGetViewportSize';
 import { userResponse } from '@/app/_slice/registerSlice';
 import useAppSelector from '@/app/_hooks/useAppSelector';
 import useAppDispatch from '@/app/_hooks/useAppDispatch';
 import { registerActions } from '@/app/_slice/registerSlice';
-import { dashBoardActions, dashBoardData } from '@/app/_slice/dashBoardSlice';
-import SideMenu from '../mydashboard/_components/SideMenu';
 import MypageHeader from './_components/mypageHeader/MypageHeader';
 import ChangePasswordDiv from './_components/changePasswordDIv/ChangePasswordDiv';
 import LeftArrow from '../../../../public/assets/icons/leftArrow.svg';
 import styles from './page.module.css';
 import EditProfileDiv from './_components/editProfileDiv/EditProfileDiv';
-import { Suspense } from 'react';
+import Loading from './Loading';
 
 const Page = () => {
   const dispatch = useAppDispatch();
-  const dashBoardDatas = useAppSelector(dashBoardData);
   const userData = useAppSelector(userResponse).data;
 
   useEffect(() => {
     dispatch(registerActions.asynchFetchgetUserInfo());
-    dispatch(dashBoardActions.asynchFetchGetDashBoard(1));
   }, [dispatch]);
 
   const viewportSize: string = useGetViewportSize();
@@ -47,28 +44,27 @@ const Page = () => {
 
   return (
     <div>
-      <Suspense>
-        {dashBoardDatas && userData && (
-          <div>
-            <SideMenu dashBoardData={dashBoardDatas?.dashboards} />
-            <MypageHeader
-              nickName={userData.nickname}
-              profileImage={userData.profileImageUrl}
-            />
-            <div className={styles.container}>
-              <div className={styles.goBackDiv}>
-                <LeftArrow />
-                돌아가기
-              </div>
-              <EditProfileDiv
-                inputWidth={EditProfileinputWidth}
-                userData={userData}
-              />
-              <ChangePasswordDiv inputWidth={changePasswordInputWidth} />
+      {userData ? (
+        <div>
+          <MypageHeader
+            nickName={userData.nickname}
+            profileImage={userData.profileImageUrl}
+          />
+          <div className={styles.container}>
+            <div className={styles.goBackDiv}>
+              <LeftArrow />
+              돌아가기
             </div>
+            <EditProfileDiv
+              inputWidth={EditProfileinputWidth}
+              userData={userData}
+            />
+            <ChangePasswordDiv inputWidth={changePasswordInputWidth} />
           </div>
-        )}
-      </Suspense>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
