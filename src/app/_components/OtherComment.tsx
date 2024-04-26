1112e8dd490d34e4c87a6bd89c12bfbfdacac908
JeongSabibi <jsbb528@naver.com>
2024-04-26 17:20:04 +0900

    feat: 댓글 수정 기능 구현

--- src/app/_components/OtherComment.tsx
+++ src/app/_components/OtherComment.tsx
@@ -1,23 +1,99 @@
-import React, { forwardRef } from 'react';
+import React, {
+  ChangeEvent,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import styles from './OtherComment.module.css';
 import { OtherCommentProps } from '../_types/otherCommentProps';
 import UserIcon from './UserIcon';
 
 const OtherComment = forwardRef<HTMLDivElement, OtherCommentProps>(
-  ({ writer, content, date, deleteComment, commentId }, ref) => {
+  (
+    {
+      writer,
+      writerProfile,
+      content,
+      date,
+      deleteComment,
+      commentId,
+      updateComment,
+    },
+    ref,
+  ) => {
+    const commentRef = useRef<HTMLTextAreaElement>(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedContent, setEditedContent] = useState(content);
+
+    const handleEdit = () => {
+      setIsEditing(true);
+    };
+
+    const handleEditSave = () => {
+      // 수정 후 저장!
+      updateComment(editedContent, commentId);
+      setIsEditing(false);
+    };
+
+    const commentInputChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      setEditedContent(e.target.value);
+    };
+
+    useEffect(() => {
+      if (!isEditing) {
+        setEditedContent(content);
+      }
+    }, [isEditing, content]);
+
     return (
       <div className={styles.otherCommentDiv} ref={ref}>
-        <UserIcon nickname={writer} profileImageUrl={null} />
+        <UserIcon nickname={writer} profileImageUrl={writerProfile} />
         <div>
           <div className={styles.userInfoDiv}>
             <p id={styles.useName}>{writer}</p>
             <p id={styles.date}>{date}</p>
           </div>
-          <div className={styles.contentDiv}>{content}</div>
+          {isEditing ? (
+            <div className={styles.commentBox}>
+              <textarea
+                placeholder={content}
+                value={editedContent}
+                className={styles.otherCommentTextarea}
+                onChange={commentInputChangeHandler}
+                ref={commentRef}
+              />
+              {editedContent && (
+                <button
+                  type="button"
+                  className={styles.inputButton}
+                  onClick={handleEditSave}
+                >
+                  입력
+                </button>
+              )}
+            </div>
+          ) : (
+            <div className={styles.contentDiv}>{content}</div>
+          )}
           <div className={styles.commentOptionsDiv}>
-            <button className={styles.option} type="button">
-              수정
-            </button>
+            {isEditing ? (
+              <button
+                className={styles.option}
+                type="button"
+                onClick={() => setIsEditing(false)}
+              >
+                취소
+              </button>
+            ) : (
+              <button
+                className={styles.option}
+                type="button"
+                onClick={handleEdit}
+              >
+                수정
+              </button>
+            )}
             <button
               className={styles.option}
               type="button"
