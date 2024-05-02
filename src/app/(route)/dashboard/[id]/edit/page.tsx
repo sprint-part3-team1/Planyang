caee7fb41d06459151beeb8ef8c765c6c21cfc00
beom <gb3187@naver.com>
2024-05-02 20:06:17 +0900

    refactor : 불필요한 코드 제거!!

--- src/app/(route)/dashboard/[id]/edit/page.tsx
+++ src/app/(route)/dashboard/[id]/edit/page.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import EditDashName from '@/app/(route)/mydashboard/_components/EditDashName';
 import TableInvite from '@/app/(route)/mydashboard/_components/TableInvite';
 import TableMember from '@/app/(route)/mydashboard/_components/TableMember';
 import BackDashBoardButton from '@/app/_components/Button/BackDashBoardButton/BackDashBoardButton';
 import DeleteDashBoardButton from '@/app/_components/Button/DeleteDashBoardButton/DeleteDashBoardButton';
-import useAppSelector from '@/app/_hooks/useAppSelector';
-import { dashBoardDetailData } from '@/app/_slice/dashBoardDetail';
+
 import styles from './style/page.module.css';
 
 const DashBoardeditPage = () => {
-  const dashboardDetailDatas = useAppSelector(dashBoardDetailData);
-
   return (
     <div className={styles.container}>
       <BackDashBoardButton />
